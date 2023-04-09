39bd19d393ccefbbb723da09808d7b0a5ea4586a
lekasNet <lekas1988@gmail.com>
2023-04-09 16:55:49 +0300

    Project Environment changed from DirectX to SFML and version MinGW changed from 9.0 to 7.3.0

--- main.cpp
+++ main.cpp
@@ -1,48 +1,24 @@
-#include "CrossWindow/CrossWindow.h"
-#include "Renderer.h"
+#include <SFML/Graphics.hpp>
 
+int main()
+{
+    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
+    sf::CircleShape shape(100.f);
+    shape.setFillColor(sf::Color::Green);
 
-void xmain(int argc, const char** argv) {
-    // Создать окно
-    xwin::WindowDesc wdesc;
-    wdesc.title = "DirectX 12 Seed";
-    wdesc.name = "MainWindow";
-    wdesc.visible = true;
-    wdesc.width = 640;
-    wdesc.height = 640;
-    wdesc.fullscreen = false;
-    xwin::Window window;
-    xwin::EventQueue eventQueue;
-    if (!window.create(wdesc, eventQueue)) { return; };
-    // Создать рендерер
-    Renderer renderer(window);
-    // Цикл движка
-    bool isRunning = true;
-    while (isRunning) {
-        bool shouldRender = true;
-        // Обновить очередь событий
-        eventQueue.update();
-        // Итерация по этой очереди:
-        while (!eventQueue.empty()) {
-            // Обновить события
-            const xwin::Event& event = eventQueue.front();
-            // При изменении размера:
-            if (event.type == xwin::EventType::Resize) {
-                const xwin::ResizeData data = event.data.resize;
-                renderer.resize(data.width, data.height);
-                shouldRender = false;
-            }
-            // При закрытии:
-            if (event.type == xwin::EventType::Close) {
+    while (window.isOpen())
+    {
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
                 window.close();
-                shouldRender = false;
-                isRunning = false;
-            }
-            eventQueue.pop();
         }
-        // Обновление визуалов
-        if (shouldRender) {
-            renderer.render();
-        }
+
+        window.clear();
+        window.draw(shape);
+        window.display();
     }
+
+    return 0;
 }
